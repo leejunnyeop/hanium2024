1c278219ff0a7c9013100ab60e18501d0e8cdb55
jihak <zhzhk17@gmail.com>
2024-05-11 00:22:22 +0900

    Feat: log4j2 설정

--- src/main/java/com/example/mypet/controller/HelloController.java
+++ src/main/java/com/example/mypet/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.mypet.controller;
 
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class HelloController {
     @GetMapping("/mypet/v1")
     public String HelloWorld(){
+        log.info("hello world");
+        log.warn("hello world");
+        log.fatal("hello world");
+        log.error("hello world");
         return "Hello World";
     }
 }
