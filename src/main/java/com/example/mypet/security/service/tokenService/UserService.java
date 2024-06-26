371062c180836007d5c74fb227cfadd756dd48a4
leejunnyeop <daffodil8620@naver.com>
2024-06-26 23:19:33 +0900

    [feat] : OAuth 에게 엑세스 토큰 요청

--- src/main/java/com/example/mypet/security/service/tokenService/UserService.java
+++ src/main/java/com/example/mypet/security/service/tokenService/UserService.java
@@ -1,2 +1,10 @@
-package com.example.mypet.security.service.tokenService;public interface UserService {
+package com.example.mypet.security.service.tokenService;
+
+import com.example.mypet.security.domain.oauth.OAuth2UserInfo;
+import com.example.mypet.security.domain.users.UsersDto;
+
+public interface UserService {
+
+    UsersDto processUser(OAuth2UserInfo userInfo, String provider);
+    OAuth2UserInfo getOAuth2UserInfo(String provider, String accessToken);
 }
