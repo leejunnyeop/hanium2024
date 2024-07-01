1025dfa9553c7309195b657bd4a49cfcf89a67ab
easyhak <jooin2000@naver.com>
2024-07-01 15:34:58 +0900

    Docs: swagger docs

--- src/main/java/com/example/mypet/security/controller/SocialAuthController.java
+++ src/main/java/com/example/mypet/security/controller/SocialAuthController.java
@@ -5,6 +5,12 @@ import com.example.mypet.security.domain.refresh.OAuth2AccessTokenResponse;
 import com.example.mypet.security.domain.refresh.RefreshTokenRequest;
 import com.example.mypet.security.domain.refresh.SocialAuthRequest;
 import com.example.mypet.security.service.tokenService.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth/social")
 @RequiredArgsConstructor
+@Tag(name = "social", description = "소셜 로그인 관련 API")
 @Log4j2
 public class SocialAuthController {
 
     private final AuthService authService;
 
 
+    @Operation(summary = "소셜 로그인", description = "",
+            responses = {@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema())))})
     @PostMapping("/social/callback")
     public ResponseEntity<AuthResponse> handleOAuth2Callback(@RequestBody SocialAuthRequest request) {
         log.info("/social/callback 실행중");
@@ -29,6 +38,7 @@ public class SocialAuthController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "JWT refresh", description = "Access Token 재발급")
     @PostMapping("/refresh")
     public ResponseEntity<OAuth2AccessTokenResponse> refreshAccessToken(@RequestBody RefreshTokenRequest request) {
         OAuth2AccessTokenResponse response = authService.refreshAccessToken(request.getRefreshToken());
