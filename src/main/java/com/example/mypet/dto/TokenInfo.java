a10c182dd8ef560cf4198f42377e76b8af6fb709
Lee JI HAK <jooin2000@naver.com>
2024-07-12 20:34:34 +0900

    Docs

--- src/main/java/com/example/mypet/dto/TokenInfo.java
+++ src/main/java/com/example/mypet/dto/TokenInfo.java
@@ -1,5 +1,6 @@
 package com.example.mypet.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -7,11 +8,13 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenInfo {
+    @Schema(example = "Bearer")
     private String grantType;
     private String accessToken;
     private Long accessTokenExpirationTime;
     private String refreshToken;
     private Long refreshTokenExpirationTime;
     @Setter
-    private Boolean isFirstLogin;
+    @Schema(example = "true", description = "처음 로그인/회원가입 여부 체크")
+    private Boolean isFirstLogin = false;
 }
