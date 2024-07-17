8984182c2a5b1feabb01b32e2feeaec73b12ca9a
easyhak <jooin2000@naver.com>
2024-07-18 01:18:47 +0900

    docs

--- src/main/java/com/example/mypet/pet/controller/PetController.java
+++ src/main/java/com/example/mypet/pet/controller/PetController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/pets")
-@Tag(name = "Pet Controller", description = "사용자 펫 관리 API" )
+@Tag(name = "pet", description = "사용자 펫 관리 API" )
 public class PetController {
 
     private final PetService petService;
