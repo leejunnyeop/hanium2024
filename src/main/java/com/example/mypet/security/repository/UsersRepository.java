b12419e39eb32ff205038587a911daca9cfcb78d
leejunnyeop <daffodil8620@naver.com>
2024-07-20 16:42:33 +0900

    [chore] 리포토리 위치찾기 메소드 구현

--- src/main/java/com/example/mypet/security/repository/UsersRepository.java
+++ src/main/java/com/example/mypet/security/repository/UsersRepository.java
@@ -3,11 +3,14 @@ package com.example.mypet.security.repository;
 import com.example.mypet.security.domain.users.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsersRepository extends MongoRepository<Users, String> {
     Optional<Users> findByName(String username);
     Optional<Users> findByEmail(String email);
+
+    List<Users> findByLocation(String location);
 
 
 
